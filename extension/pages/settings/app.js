8d7c8cec981a15f44721982454c67eb6ac1bff86
treemmett <treemmett@gmail.com>
2017-09-04 08:56:42 -0600

    Added credential saving

--- extension/pages/settings/app.js
+++ extension/pages/settings/app.js
@@ -1,3 +1,19 @@
+//Save XID
+document.getElementById('xid_save').addEventListener('click', function(){
+  chrome.storage.local.set({xid: {
+    user: document.getElementById('xid_user').value.trim(),
+    pass: document.getElementById('xid_pass').value.trim()
+  }}, message('changes saved'));
+});
+
+//Save Blue Tool
+document.getElementById('bt_save').addEventListener('click', function(){
+  chrome.storage.local.set({bt: {
+    user: document.getElementById('bt_user').value.trim(),
+    pass: document.getElementById('bt_pass').value.trim()
+  }}, message('changes saved'));
+});
+
 function message(msg){
   const m = document.createElement('div');
   m.classList.add('message', 'hide');
