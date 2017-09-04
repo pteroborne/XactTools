06bc3be898186d7e9639b77e880808e6dd184892
treemmett <treemmett@gmail.com>
2017-09-04 11:33:20 -0600

    Added username auto load

--- extension/pages/settings/components/credentials/app.js
+++ extension/pages/settings/components/credentials/app.js
@@ -1,3 +1,18 @@
+//Load stored usernames
+chrome.storage.local.get(['xid', 'bt'], function(i){
+  if(i.xid){
+    if(i.xid.user){
+      document.getElementById('xid_user').value = i.xid.user;
+    }
+  }
+
+  if(i.bt){
+    if(i.bt.user){
+      document.getElementById('bt_user').value = i.bt.user;
+    }
+  }
+});
+
 //Save XID
 document.getElementById('xid_save').addEventListener('click', function(){
   chrome.storage.local.set({xid: {
