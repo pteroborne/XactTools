c51035b1b1e52b81d0a947f134a2a0cd03e1db2e
treemmett <treemmett@gmail.com>
2017-10-22 16:24:04 -0600

    Added toast removal

--- src/Toast.js
+++ src/Toast.js
@@ -2,12 +2,48 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import './Toast.scss';
 
-export default class Parent extends Component{
+export default class ToastContainer extends Component{
+  constructor(){
+    super();
+    this.state = {toasts: [
+      {message: 'This is a toast!', display: true},
+      {message: 'This is a very long toast. Look how long this toast is. Damn.', display: true}
+    ]}
+  }
+
+  //Test method to generate new toast
+  componentDidMount(){
+    setTimeout(()=>{
+      this.setState({toasts: this.state.toasts.concat({message: 'This is a new toast', display: true})});
+    }, 1050);
+  }
+
+  removeToast = (i)=>{
+    //Find transition time of fade out
+    const time = getComputedStyle(ReactDOM.findDOMNode(i)).transition.match(/margin-top ([0-9]+)/)[1] * 1000;
+
+    //Start timer until removal
+    setTimeout(()=>{
+      //Store current toasts
+      const toasts = this.state.toasts;
+      toasts[i.props.id].display = false;
+      this.setState({toasts: toasts});
+    }, time);
+  }
+
   render(){
+    let renderedToasts = [];
+
+    //Render each toast in state
+    for(let i = 0; i < this.state.toasts.length; i++){
+      if(this.state.toasts[i].display){
+        renderedToasts.push(<Toast message={this.state.toasts[i].message} key={i} id={i} removeToast={this.removeToast}/>);
+      }
+    }
+
     return(
       <div className="notifications">
-        <Toast message="This is a toast!"/>
-        <Toast message="This is a very long toast. Look how long this toast is. Damn."/>
+        {renderedToasts}
       </div>
     );
   }
@@ -51,7 +87,7 @@ class Toast extends Component{
     this.setState({style: {
       marginTop: '-'+height+'px',
       boxShadow: 'none'
-    }});
+    }}, ()=>{this.props.removeToast(this)});
   }
 
   render(){
